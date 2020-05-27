74093861ba6a33bd276a66be7fafc7f2366f69f0
Pratyush M <mpratyush2008@gmail.com>
2020-05-27 19:56:49 +0530

    Added 6-B

---
+++ Program - 6/sample_input.c
@@ -0,0 +1,10 @@
+// Comment line -1
+/* C program to find sum of two numbers */
+
+#include<stdio.h>
+int main()
+{
+    int a = 2, b = 3;
+    result = a+b;
+    printf("The sum is: %d", result);
+}
